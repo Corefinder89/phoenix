aefbfd759101c63a62ba84f0a5d53a5b2b3dc456
Corefinder89 <soumyajit.basu1988@outlook.com>
2021-07-15 21:10:26 +0530

    Adding project3 to repo

---
+++ simplilearn-phase1/ci-cd(Project-3)/docker/src/test/java/com/couchbase/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couchbase.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
