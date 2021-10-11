f85cc5f8bd36b151310ebde90bf9edf7137a18aa
Corefinder89 <soumyajit.basu1988@outlook.com>
2021-10-11 16:21:37 +0530

    add network and change deployment script

--- capstone/deployment.sh
+++ capstone/deployment.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# goto flask weather app
-cd flask-weather-app/
+status=$( sudo docker images -q corefinder/app-runner )
 
 # Check if the app-runner image is already existing
-if [ -z "$(docker images -q corefinder/app-runner)" ]; then
-    echo "Image already exist"
+if [[ -n "$status" ]]; then
+  echo "Container exists"
 else
-    docker build -t corefinder/app-runner app-runner/
+  docker build -t corefinder/app-runner app-runner/
 fi
 
+# Check if public ip address is set as environmetn variabe
+if [ -z ${server_ip} ];
+then
+    export server_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+else
+    echo "environmetn variable already present"
+fi
+
+# goto flask weather app
+cd flask-weather-app/
 docker-compose up
